3e1a8818649ebdf211722d8cd62c367f96a467d8
Natalia Firstova <113723377+fitalo4ka@users.noreply.github.com>
2023-04-03 16:28:21 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,15 +3,16 @@
 
 int main() {
     int a, size;
+    std::cout << "Enter the size of the list: ";
     std::cin >> size;
     std::cout << "Enter the values for the list: ";
     std::vector<int> val(size);
-    for(auto i = val.begin(); i != val.end(); ++i) {
+    for(std::vector<int>::iterator i = val.begin(); i != val.end(); ++i) {
         std::cin >> a;
         *i = a;
     }
     std::cout << std::endl << "Reversed list: ";
-    for(std::vector <int>::reverse_iterator i = val.rbegin(); i != val.rend(); ++i) {
+    for(std::vector<int>::reverse_iterator i = val.rbegin(); i != val.rend(); ++i) {
         std::cout << *i << " ";
     }
     return 0;
